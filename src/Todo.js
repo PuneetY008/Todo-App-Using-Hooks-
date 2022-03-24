47763d8e8219ad1069c77b6a4e9b0e5559b9bd9c
Puneet Yadav <puneetyadav380@gmail.com>
2022-03-24 18:43:40 +0530

    added memo

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, memo } from "react";
 import {
   ListItem,
   ListItemText,
@@ -47,4 +47,4 @@ function Todo({ task, completed, id }) {
   );
 }
 
-export default Todo;
+export default memo(Todo);
